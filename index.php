63862b5247e8f4b763b20eec6bc66193b930bf17
Roko <capitalcatto@gmail.com>
2024-10-14 22:30:55 -0300

    Agregados Mantenedores Notebook
    Agregados Mantenedores Notebook

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar With Bootstrap</title>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <i class="lni lni-grid-alt"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">Tisnology</a>
+                </div>
+            </div>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                        data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+                    <i class="lni lni-protection"></i>
+                    <span>Teclado</span>
+                </a>
+                <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                            <a href="mantenedores_periferico/categoria_teclado.php" class="sidebar-link">Categoria teclado</a>      
+                    </li>
+                    <li class="sidebar-item">
+                            <a href="mantenedores_periferico/tipo_teclado.php" class="sidebar-link">Tipo teclado</a>      
+                    </li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                        data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+                    <i class="lni lni-protection"></i>
+                    <span>Notebooks</span>
+                </a>
+                <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                            <a href="mantenedores_notebook/cpu_notebook.php" class="sidebar-link">Cpu Notebook</a>      
+                    </li>
+                    <li class="sidebar-item">
+                            <a href="mantenedores_notebook/gpu_notebook.php" class="sidebar-link">Gpu Notebook</a>      
+                    </li>
+                    <li class="sidebar-item">
+                            <a href="mantenedores_notebook/pantalla_notebook.php" class="sidebar-link">Pantalla Notebook</a>      
+                    </li>
+                    <li class="sidebar-item">
+                            <a href="mantenedores_notebook/bateria_notebook.php" class="sidebar-link">Batería Notebook</a>      
+                    </li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                    <a href="crear_producto.php" class="sidebar-link">
+                        <i class="lni lni-user"></i>
+                        <span>Crear producto</span>
+                    </a>
+            </li>
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </aside>
+        <div class="main p-3">
+            <div class="text-center">
+                <h1>
+                    Tisnology
+                </h1>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
