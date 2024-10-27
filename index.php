e6a8f430726fb69a24b0d76e94aac00619939b0c
Matyyp <mgonzalezp@ing.ucsc.cl>
2024-10-27 15:23:41 -0300

    Catalogo para todos los roles completo

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,32 +9,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo de Productos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
 </head>
 <body>
 
-<nav class="barra1 navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <div class="row col-2">
-      <img class="img-fluid w-75" src="https://upload.wikimedia.org/wikipedia/commons/d/df/Ripley_Logo.png" alt="">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <div class="row col-2">
+            <img class="img-fluid w-75" src="https://upload.wikimedia.org/wikipedia/commons/d/df/Ripley_Logo.png" alt="">
+        </div>
+        <div class="row col-8">
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn btn-primary" type="submit">Buscar</button>
+            </form>
+        </div>
+        <div class="row col-2 text-end">
+            <div class="d-grid gap-2 d-md-block">
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <!-- Si el usuario es admin, mostrar botón adicional -->
+                    <?php if ($_SESSION['role'] === 'admin'): ?>
+                        <button type="button" class="btn btn-primary" onclick="window.location.href='admin_panel/admin_panel.php';">Panel de Admin</button>
+                    <?php endif; ?>
+                    <button type="button" class="btn btn-secondary" onclick="window.location.href='login/logout.php';">Cerrar Sesión</button>
+                <?php else: ?>
+                    <button type="button" class="btn btn-primary" onclick="window.location.href='login/login.php';">Iniciar Sesión</button>
+                <?php endif; ?>
+            </div>
+        </div>
     </div>
-    <div class="row col-8">
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-        <button class="btn btn-primary" type="submit">Buscar</button>
-      </form>
-    </div>
-    <div class="row col-2">
-      <div class="d-grid gap-2 d-md-block">
-        <button type="button" class="btn btn-primary" onclick="window.location.href='login/login.php';">Iniciar sesion</button>
-      </div>
-    </div>
-  </div>
 </nav>
 
 <div class="container my-4">
     <div class="row d-flex justify-content-center">
-
         <?php
         // Conexión a la base de datos
         require('conexion.php');
@@ -53,19 +63,17 @@
 
         if ($result->num_rows > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
-                // Variables del producto
                 $id_producto = $row['id_producto'];
                 $marca = $row['marca']; 
                 $nombre_producto = $row['nombre_producto'];
                 $precio = number_format($row['precio'], 0, ',', '.');
                 $imagen_url = $row['imagen_url']; 
 
-                // HTML del producto con enlace al detalle
                 echo "
                   <div class='card mx-1 mb-3 p-1 shadow' style='width: 18rem;'>
                     <img src='$imagen_url' alt='$nombre_producto'>
                       <div class='card-body text-begin'>
-                        <a class='text-decoration-none' href=''>
+                        <a class='text-decoration-none' href='catalogo_productos/detalle_producto.php?id_producto=$id_producto'>
                             <p class='text-secondary'>$marca</p>
                             <h5 class='text-black'>$nombre_producto</h5>
                             <p class='text-secondary'>$$precio</p>
@@ -79,7 +87,6 @@
         }
         mysqli_close($conexion);
         ?>
-
     </div>
 </div>
 </body>
