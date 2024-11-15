6c5586a2e6fc2145e918e39b22802b9c951f0eba
ConstanzaP1 <112583850+ConstanzaP1@users.noreply.github.com>
2024-11-15 02:45:39 -0300

    arreglo ajustar imagen
    no funciona

--- creacion_productos/crear_teclado.php
+++ creacion_productos/crear_teclado.php
@@ -28,6 +28,7 @@ $resultMarca = mysqli_query($conexion, $queryMarca);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Teclado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="funcion_ajustar_imagen/ajustar_imagen">
 </head>
 <body>
 <div class="container mt-5">
