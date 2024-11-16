04dc04d495c2827f362d7c86d9c80c3bff7c6731
ConstanzaP1 <112583850+ConstanzaP1@users.noreply.github.com>
2024-11-16 02:10:41 -0300

    eliminar drag and drop comparador y filtro

--- funcion_drag_and_drop/organizacion_filtro.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents("php://input"), true);
-    $filterOrder = $data['filters'];
-    
-    // Aquí puedes guardar $filterOrder en la base de datos o en sesión, según lo necesites.
-    echo json_encode(['status' => 'success', 'order' => $filterOrder]);
-}
-?>
