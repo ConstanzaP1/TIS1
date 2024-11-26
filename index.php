0db222d53cd3296212de6c4e63317e098f0f681d
Matyyp <mgonzalezp@ing.ucsc.cl>
2024-11-26 20:51:09 -0300

    delete boton

--- index.php
+++ index.php
@@ -116,13 +116,6 @@ if (empty($marca) && empty($precio_min) && empty($precio_max) && empty($categori
                         </svg>
                     </button>
                     </li>
-                    <li class="nav-item">
-                            <button type="button" class="btn btn-comparar p-3 ms-2 rounded-pill" onclick="window.location.href='../TIS1/comparador/comparador.php'">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
-                                </svg>
-                            </button>
-                        </li>
                 <?php else: ?>
                     <li class="nav-item">
                         <a class="btn btn-primary" href="login/login.php">Iniciar Sesión</a>
