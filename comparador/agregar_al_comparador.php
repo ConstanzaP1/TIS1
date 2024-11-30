bbee5b2606c92fcbd903ffaa592144fa4a6281fd
Roko <capitalcatto@gmail.com>
2024-11-29 21:07:18 -0300

    actualizacion sweet alert comparador

--- comparador/agregar_al_comparador.php
+++ comparador/agregar_al_comparador.php
@@ -17,18 +17,26 @@ if (isset($_POST['id_producto'])) {
     $id_producto = filter_var($_POST['id_producto'], FILTER_VALIDATE_INT);
 
     if ($id_producto) {
+        // Comprobar si el producto ya está en el comparador
         if (!in_array($id_producto, $_SESSION['comparador'])) {
-            $_SESSION['comparador'][] = $id_producto;
+            $_SESSION['comparador'][] = $id_producto; // Agregar producto al comparador
             $response = [
                 'status' => 'success',
                 'message' => 'Producto agregado al comparador.'
             ];
         } else {
+            // Producto ya está en el comparador
             $response = [
                 'status' => 'exists',
                 'message' => 'El producto ya está en el comparador.'
             ];
         }
+    } else {
+        // ID del producto inválido
+        $response = [
+            'status' => 'error',
+            'message' => 'ID del producto inválido.'
+        ];
     }
 }
 
