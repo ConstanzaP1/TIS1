3a68fd1ebaaae8ed43ac924549a74411f3ac45cd
Matyyp <mgonzalezp@ing.ucsc.cl>
2024-12-01 01:58:35 -0300

    fix login navbar pequeña

--- index.php
+++ index.php
@@ -359,73 +359,71 @@ function filtrarProductosPorTipo($tipo)
         </div>
         <div class="offcanvas-body">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle bg-white rounded-pill p-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Bienvenid@, <?php echo htmlspecialchars($_SESSION['username']); ?>!
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <?php if (in_array($_SESSION['role'], ['admin', 'superadmin'])): ?>
-                            <li>
-                                <a class="dropdown-item" href="admin_panel/admin_panel.php">Panel Admin</a>
-                            </li>
-                        <?php endif; ?>
-                        <li>
-                            <a class="dropdown-item text-black" href="perfil_usuario/perfil_usuario.php">Mi perfil</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item text-danger" href="login/logout.php">Cerrar Sesión</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle bg-white rounded-pill p-3" type="button" id="productosDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        Categorias
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="productosDropdown">
-                        <!-- Opción para todos los productos -->
-                        <li>
-                            <a class="dropdown-item" href="catalogo_productos/catalogo.php">Todos los productos</a>
-                        </li>
-                        <?php 
-                        // Opciones dinámicas basadas en tipos de producto
-                        $tiposDeProducto = obtenerTiposDeProducto();
-                        foreach ($tiposDeProducto as $tipo): ?>
-                            <li>
-                                <a class="dropdown-item text-capitalize" href="catalogo_productos/catalogo.php?tipo_producto=<?php echo urlencode($tipo); ?>">
-                                    <?php echo htmlspecialchars($tipo); ?>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </li>
                 <?php if (isset($_SESSION['user_id'])): ?>
-                <div class="d-flex">
-                <li class="nav-item">
-                    <button type="button" class="btn btn-cart p-3 rounded-pill" onclick="window.location.href='carrito/carrito.php'">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                        </svg>
-                    </button>
-                </li>
-                <li class="nav-item">
-                    <button type="button" class="btn btn-comparar p-3 ms-2 rounded-pill me-2" onclick="window.location.href='../TIS1/comparador/comparador.php'">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
-                        </svg>
-                    </button>
-                </li>
-                <li>
-                    <button type="button" class="btn btn-deseos p-3 ms-2 rounded-pill me-2" onclick="window.location.href='lista_deseos/lista_deseos.php'">
-                    <i class='fas fa-heart'></i>
-                    </button>
-                </li>
-                </div> 
-                
-                
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle bg-white rounded-pill p-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Bienvenid@, <?php echo htmlspecialchars($_SESSION['username']); ?>!
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <?php if (in_array($_SESSION['role'], ['admin', 'superadmin'])): ?>
+                                <li>
+                                    <a class="dropdown-item" href="admin_panel/admin_panel.php">Panel Admin</a>
+                                </li>
+                            <?php endif; ?>
+                            <li>
+                                <a class="dropdown-item text-black" href="perfil_usuario/perfil_usuario.php">Mi perfil</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-danger" href="login/logout.php">Cerrar Sesión</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle bg-white rounded-pill p-3" type="button" id="productosDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categorias
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="productosDropdown">
+                            <!-- Opción para todos los productos -->
+                            <li>
+                                <a class="dropdown-item" href="catalogo_productos/catalogo.php">Todos los productos</a>
+                            </li>
+                            <?php 
+                            // Opciones dinámicas basadas en tipos de producto
+                            $tiposDeProducto = obtenerTiposDeProducto();
+                            foreach ($tiposDeProducto as $tipo): ?>
+                                <li>
+                                    <a class="dropdown-item text-capitalize" href="catalogo_productos/catalogo.php?tipo_producto=<?php echo urlencode($tipo); ?>">
+                                        <?php echo htmlspecialchars($tipo); ?>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <div class="d-flex">
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-cart p-3 rounded-pill" onclick="window.location.href='carrito/carrito.php'">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                                </svg>
+                            </button>
+                        </li>
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-comparar p-3 ms-2 rounded-pill me-2" onclick="window.location.href='../TIS1/comparador/comparador.php'">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
+                                </svg>
+                            </button>
+                        </li>
+                        <li>
+                            <button type="button" class="btn btn-deseos p-3 ms-2 rounded-pill me-2" onclick="window.location.href='lista_deseos/lista_deseos.php'">
+                            <i class='fas fa-heart'></i>
+                            </button>
+                        </li>
+                    </div>       
                 <?php else: ?>
-                <li class="nav-item">
-                    <a class="btn btn-primary" href="login/login.php">Iniciar Sesión</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary col-12" href="login/login.php">Iniciar Sesión</a>
+                    </li>
                 <?php endif; ?>
             </ul>
         </div>
