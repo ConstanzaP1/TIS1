cb345d300a4dd1972eeecdbcb597c11f2aaa23e5
Roko <capitalcatto@gmail.com>
2024-12-01 22:21:19 -0300

    Update admin_postventa.php

--- postventa/admin_postventa.php
+++ postventa/admin_postventa.php
@@ -114,6 +114,24 @@ $result = $conexion->query($query);
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../admin_panel/admin_panel.php">
+                <img src="../logoblanco.png" alt="Logo" style="width: auto; height: auto;" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../admin_panel/admin_panel.php">Volver al Panel</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 <div class="container mt-5">
     <h2 class="text-center mb-4">Administración de Postventa</h2>
     <div class="table-responsive">
@@ -241,13 +259,5 @@ $result = $conexion->query($query);
         });
     <?php endif; ?>
 </script>
-<div class="container mt-5">
-    <div class="mb-3">
-        <a href="../admin_panel/admin_panel.php" class="btn btn-secondary">
-            <i class="bi bi-arrow-left-circle"></i> Volver al Panel
-        </a>
-    </div>
-</div>
-
 </body>
 </html>
